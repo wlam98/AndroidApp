dc2acd2a7b11c00ba0c7463ee2ef5c9dd25066e3
mayilen <60578759+mayilen@users.noreply.github.com>
2020-11-02 15:51:46 +0400

    splash page

--- NotSnap/app/src/main/java/com/example/notsnap/MainActivity.java
+++ NotSnap/app/src/main/java/com/example/notsnap/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.notsnap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //abc;
+
+
     }
 }
