b167718692c2450a4512f20c5d9b0e720c5cd0be
SiddharthChadha <siddharth17499@gmail.com>
2020-11-03 04:24:43 -0500

    Adding capture button, edit mode page and their respective handlers

--- NotSnap/app/src/main/java/com/example/notsnap/MainActivity.java
+++ NotSnap/app/src/main/java/com/example/notsnap/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+    }
+
+    public void captureButtonHandler(View view) {
+        Toast.makeText(this, "capture clicked", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.edit_mode);
+    }
+
+    public void discardButtonHandler(View view) {
+        setContentView(R.layout.activity_main);
     }
 }
